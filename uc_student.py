c59c00d1191dede9701f8009ea0ec19813996bec
Matthew Crabtree <155996136+VoidWalkerZ7@users.noreply.github.com>
2024-02-09 14:28:23 -0500

    Update uc_student.py
    I updated the uc_6_2 function to be completed to help save time and difficulty on the exercise

--- uc_student.py
+++ uc_student.py
@@ -11,24 +11,19 @@ def demographics():
 
 
 
-# Fuction to return the six plus two
+# Function to return the six plus two
 # You may copy and paste the uc_6_2 function defined earlier     
 def uc_6_2(first_name,last_name):
     # Use the appropriate If and else Decision Block with a condition that compares the Length of the Last name
     # In the If and else Decision Block store the six plus two in a variable
     # Use the Tips below in your If and else Decision Block
     if len(last_name) >= 6:
-        six_plus_2 = '' # Replace this line with the right code
-        
+        sixplus2 = last_name[0:6] + first_name[0] + first_name[-1]        
     # Continue the "If..elif..else" block with the appropriate block for the remaining scenario
-    
+    else:
+      sixplus2= last_name + first_name[0:(6-len(last_name)+1)] + first_name[-1]
     # Return the six plus two in lower case
-    return
-
-# Tips (Replace the portions marked as with ?)
-# ********************************************
-# For length of the last name is >=6, sixplus2 = last_name[0:?] + first_name[0] + first_name[?]
-# For length of the last name is <6, sixplus2 = last_name + first_name[0:(6-len(?)+1)] + first_name[?]
+    return(sixplus2.lower())
 
 
 
