9d78beecafd41bfa449479165d526e14686cab72
Matthew Crabtree <155996136+VoidWalkerZ7@users.noreply.github.com>
2024-08-30 11:41:13 -0400

    Update uc_student.py
    additional spelling fixes to comments

--- MyPackage/uc_student.py
+++ MyPackage/uc_student.py
@@ -5,7 +5,7 @@ def demographics():
     year_of_birth = input('What is your Year of Birth: ')
     return [first_name, last_name, year_of_birth]
 
-# Function to return he six plus two    
+# Function to return the six plus two    
 def uc_6_2(first_name, last_name):
     if (len(last_name)>=6):
         sixplus2 = last_name[0:6] + first_name[0] + first_name[-1]
@@ -16,7 +16,7 @@ def uc_6_2(first_name, last_name):
 # Student class definition
 class Student:
     student_count = 0
-    # initialisation method
+    # initialization method
     def __init__(self, first_name, last_name, year_of_birth):
         self.first_name = first_name
         self.last_name = last_name
@@ -26,6 +26,6 @@ class Student:
     # Method to return student number (year of birth + count)
     def student_number(self):
         return self.year_of_birth + str(self.num)
-    # Metod to return student UC six plus two
+    # Method to return student UC six plus two
     def student_id(self):
         return uc_6_2(self.first_name, self.last_name)
